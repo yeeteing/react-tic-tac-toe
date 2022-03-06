bb318425e341aab47d3d1194ff6bb6d0651aa42e
yeeteing <ytlo@mun.ca>
2022-03-06 15:05:57 -0500

    able to go back to the history

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Board from "./Board";
+
 interface IProps {
 }
 
 interface IStats {
   currentSquares: Array<string>,
-  history: Array<Array<Object>>,
-  isNext: boolean
+  history: Array<Array<string>>,
+  isNext: boolean,
+  steps: number
 
 }
 
@@ -17,14 +19,15 @@ class Game extends React.Component <IProps, IStats> {
     this.state = {
       history: [],
       currentSquares: Array(9).fill(null),
-      isNext: true
+      isNext: true,
+      steps: 0
     }
   }
 
   handleClick(i: number) {
     const history = this.state.history;
     const squares = this.state.currentSquares.slice();
-    
+    const nextSteps =  this.state.steps + 1;
     // if winner is found, end game? 
     // if winner have not found, keep adding squares, history, and is next
     // and if squares i is empty
@@ -32,10 +35,10 @@ class Game extends React.Component <IProps, IStats> {
       squares[i] = this.state.isNext?'X':'O';
       this.setState({
         currentSquares: squares,
-        history: history.concat([{squares}]),
-        isNext:  !this.state.isNext
+        history: history.concat([squares]),
+        steps: nextSteps,
+        isNext:  nextSteps%2 === 0?true:false
       });
-      console.log('here')
     }
     
   }
@@ -66,6 +69,20 @@ class Game extends React.Component <IProps, IStats> {
     }
   }
 
+  backTo(i: number){
+    const history = this.state.history;
+    const nextSteps =  i;
+    const nextHistory = history.slice(0,i);
+    const nextSquares = nextHistory[nextHistory.length-1];
+
+    this.setState({
+      currentSquares: nextSquares,
+      history: nextHistory,
+      steps: nextSteps,
+      isNext:  nextSteps%2 === 0?true:false
+    });
+  }
+
   render() {
     const history = this.state.history;
     const squares = this.state.currentSquares.slice();
@@ -85,9 +102,14 @@ class Game extends React.Component <IProps, IStats> {
           <div>{/* status */}</div>
           <div>
           <ol>
+          <div>
             {this.state.history.map((element, index) => {
-              return <li key={index}>{index}</li>;
+              return <li>
+                <button key={index} onClick={()=>this.backTo(index)}>{index===0?"Go to start game":`Go to move # ${index}`}</button>
+              </li>;
             })}
+          </div>
+
           </ol>
           </div>
           
